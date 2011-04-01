df6c0b03789d1e141665dbc27ed71c48bf24fb40
NaN-from-jediNetRu <lubenin@gmail.com>
2011-04-01 13:45:05 -0700

    Edited kick.js via GitHub

--- kick.js
+++ kick.js
@@ -8,7 +8,7 @@ var vhosts = [
 "<iframe title=\"YouTube video player\" width=\"425\" height=\"349\" src=\"http://www.youtube.com/embed/$5\" frameborder=\"0\" allowfullscreen></iframe>"
 ],
 [
-/((http\:\/\/)?(www\.)?rutube\.ru\/tracks\/\d+\.html\?v\=)(.*)(\&.*$)?/gim , 
+/((http\:\/\/)?(www\.)?rutube\.ru\/tracks\/\d+\.html\?v\=)([a-zA-Z0-9\-]+)(\&.*)?$/gim , 
 "<OBJECT width=\"470\" height=\"353\"><PARAM name=\"movie\" value=\"http://video.rutube.ru/$4\"></PARAM><PARAM name=\"wmode\" value=\"window\"></PARAM><PARAM name=\"allowFullScreen\" value=\"true\"></PARAM><EMBED src=\"http://video.rutube.ru/$4\" type=\"application/x-shockwave-flash\" wmode=\"window\" width=\"470\" height=\"353\" allowFullScreen=\"true\" ></EMBED></OBJECT>"
 ]				
                  ];
