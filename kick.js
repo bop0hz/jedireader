433ef92c7ce9b69c9a67c386780322d4e024591d
БопоХ <boppy@bk.ru>
2012-05-02 00:33:38 +0400

    bootstrap css, редизайн, новая система оповещений (иконка+Desktop Notifications)

--- kick.js
+++ kick.js
@@ -14,42 +14,42 @@ var vhosts = [
 			];
 
 $(document).ready(function(){
-	
+
 	$("textarea[name='req_message']").bind("keypress",function(event){
-	
+
 		var ev = event||window.event;
 		var kk = ev.keyCode;
 		var ctrl = ev.ctrlKey;
-		
+
 		if ((kk==10||kk==13) && ctrl) 
 			$("form[name='post'] input[type='submit']").get(0).click();
 
 	}); 
-	
+
 	$(".postmsg p").each(function(){
-	
+
 		$(this).html($(this).html().replace(/(\{\#jedicut\})(.+)$/gim,'<a href="javascript:void(0)" onclick="var cut =  this.parentNode.getElementsByClassName(\'jedicut\')[0]; if(cut.offsetWidth>0)cut.style.display=\'none\'; else cut.style.display=\'block\'">читать/не читать далее</a><div class="jedicut" style="display:none">$2</div>'));
 
-	
+
 	});
-	
-	
+
+
 	$(".postmsg a").each(function(){
-	
-		
+
+
 		var msg = $(this).attr("href");
-		
+
 		for(var i=0,j=vhosts.length;i<j;i++) {
-			
+
 			if(vhosts[i][0].test(msg)){
-		
+
 			msg = msg.replace(vhosts[i][0],vhosts[i][1]);
-			
+
 			$(this).replaceWith(msg);
-			
+
 			}
-			
-		
+
+
 		}
 
 	});
@@ -58,37 +58,7 @@ $(document).ready(function(){
 	var body = {
 		"font-family":"Verdana, Helvetica, sans-serif",
 	}
-	var messageBoard = {
-		"display":"block",
-		"color":"#fff",
-		"top":"0",
-		"right":"0",
-		"position":"fixed",
-		"margin":"20",
-		"text-align":"center",
-		"text-decoration":"none",
-		"text-shadow":"1px 1px 1px #000",
-		"text-decoration":"none",
-		"width":"200",
-		"margin":"auto",
-		"background-color":"#f86248",
-		"opacity":".7",
-		"border":"5px solid #444",
-		"z-index":"100",
-	}
 	$("body").css(body);
-
-// оповещение
-	var privateMessages = document.getElementById("brdmenu").innerHTML;
-	var regexp = /\<b\> \[\d+\]\<\/b\>/i;
-	if (regexp.test(privateMessages)) {
-		$("body").prepend("\
-			<a id=\"messageBoard\" href=\"http://jedi.net.ru/message_list.php\">\
-				Непрочитанное сообщение\
-			</a>\
-		");
-	};
-	$("#messageBoard").css(messageBoard);
 
 // bbcode
 	$(".txtarea").prepend("\
@@ -156,7 +126,7 @@ $(document).ready(function(){
 			$(".linksb + .box").remove();
 		};
 	});
-	
+
 //удаление веток			
 	chrome.extension.sendRequest({localstorage: "branches"}, function(response){
 		var kickedBranches = response["branches"].split('###');
@@ -166,36 +136,36 @@ $(document).ready(function(){
 	});
 
 	chrome.extension.sendRequest({localstorage: "kicked"}, function(response) {
-		
+
 		/**/
-				
+
 		/**/
 		var st = response.kicked.split("=#####=");
-	
-		
+
+
 	$("body").append("<div id='popupp' style='position:absolute;padding:9px;-webkit-border-radius:10px;font-size:12px;background:#EEE;-webkit-box-shadow:0 0 3px 4px #CCC;display:none;z-index:5000;'><a id='to_s' style='color:red' href='javascript:;' onclick='addToStorage(current_pretendent,st)'>В список!</a><br /><br /><a style='color:green' href='javascript:;' id='from_s'>Реабилитировать</a></div>");
-	
+
 	$("#to_s").click(function() {
 
 	var uy = current_pretendent;
 	var lst = st;
 	var lsst = st.join('=#####=');
 	if ( !in_array(uy,lst) && uy!="" ){
-		
+
 		var t =(lsst=="" ? '':lsst+'=#####=')+uy;
 			lsst = t;
 			chrome.extension.sendRequest({localstorage: "set",settedValue:lsst},function(response){document.location.reload()});
-	
+
 		} else {
-			
-			alert('Эта чучундра уже в списке!');
-			
+
+			alert('Уже в списке!');
+
 		}
 
 	});
-	
-	
-	
+
+
+
 	function a_index(arr,fin){
 		for(var i=0,l=arr.length;i<l;i++){
 			if (arr[i]==fin) return i;
@@ -208,17 +178,17 @@ $(document).ready(function(){
 	if (in_array(uy,lst) && uy!=""){
 		var aj = a_index(lst,uy);
 		if(aj>=0) delete lst[aj];
-		
+
 			chrome.extension.sendRequest({localstorage: "set",settedValue:lst.join('=#####=')},function(response){document.location.reload()});
-	
+
 		} else {
-			
+
 			alert('Его и так там нету!');
-			
+
 			}
 			});
-		
-		
+
+
 		$("div.blockpost a[href*='profile.php']").bind("contextmenu",function(event){
 		var target = (event||window.event);
 		var x = target.clientX+document.body.scrollLeft+5;
@@ -226,21 +196,21 @@ $(document).ready(function(){
 		current_pretendent = $(this).text()||"";
 		$("#popupp").css({'left':x+'px','top':y+'px'}).show();
 		$("div.box").click(function(){
-			
+
 				var ob = $("#popupp");
-			
+
 				if (ob.is(':visible')) 
 					ob.hide("fast"); 
-					
+
 			});		
 		//if (in_array(text,st)	&& text!="")
-		
+
 		return false});
-		
+
 			$("div.blockpost a[href*='profile.php']").each(function(){
-				
+
 				var text = $(this).text()||"";
-				
+
 				if (in_array(text,st)	&& text!="") {
 					$(this)
 					.parents("div.blockpost")
@@ -259,45 +229,45 @@ $(document).ready(function(){
 					.parents("div.blockpost")
 					.children("div.box")
 					.hide();
-					
+
 				} 
 			});
-			
-			
-			
-			
+
+
+
+
 			$("blockquote").each(function(){
-			
+
 				var text = $(this).find("h4").text().replace(/\s+написал\:/gim,'')||"";
-				
+
 				if(in_array(text,st) && text!="") $(this).css({'opacity':'0.1'});
-			
+
 			});
-			
-			
+
+
 			$(".byuser").each(function(){
-			
+
 				var text = $(this).text().replace(/^от\s+/gim,'')||"";
-			
+
 				if (in_array(text,st) && $(this).parents("td").is(":first-child") && text!="") 
 					 $(this)
 					 .parents("tr")
 					 .children("td")
 					 .css({'opacity':'0.1'});
-			
+
 			});
-	
-	
+
+
 			$(".shhd").click(function(){
-			
+
 				var ob = $(this).parents("div.blockpost").children("div.box");
-			
+
 				if (ob.is(':visible')) 
 					ob.hide(); 
 				else 
 					ob.show('fast');
-			
+
 			});
-			
-	
+
+
 	});
